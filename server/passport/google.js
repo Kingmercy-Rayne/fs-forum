25dbd4270f5ec5cafd62d44ff3ca9115aba280ad
Kingmercy-Rayne <kingrayne66@gmail.com>
2020-04-27 22:21:27 +0800

    Add correct dependency for oauth: passport-google-oauth2

--- server/passport/google.js
+++ server/passport/google.js
@@ -1,31 +1,13 @@
-var passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth").Strategy;
+const passport = require("passport");
 
-// Use the GoogleStrategy within Passport.
-//   Strategies in passport require a `verify` function, which accept
-//   credentials (in this case, a token, tokenSecret, and Google profile), and
-//   invoke a callback with a user object.
-passport.use(
-  new GoogleStrategy(
-    {
-      consumerKey: process.env.GOOGLE_CONSUMER_KEY,
-      consumerSecret: process.env.GOOGLE_CONSUMER_SECRET,
-      callbackURL: "/auth/google/callback",
-    },
-    function (token, tokenSecret, profile, done) {
-      console.log(profile);
-      return cb(new Error("Working on it....."));
-    }
-  )
-);
 var GoogleStrategy = require("passport-google-oauth2").Strategy;
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.GOOGLE_CONSUMER_KEY,
-      clientSecret: process.env.GOOGLE_CONSUMER_SECRET,
-      callbackURL: "http://yourdormain:3000/auth/google/callback",
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
       passReqToCallback: true,
     },
     function (request, accessToken, refreshToken, profile, done) {
