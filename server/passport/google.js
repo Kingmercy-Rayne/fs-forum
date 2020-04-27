d2e39e0f5ea6a7f1c4eda7029ed9832b337a87d6
Kingmercy-Rayne <kingrayne66@gmail.com>
2020-04-27 22:01:55 +0800

    Update to Google Oauth2

--- server/passport/google.js
+++ server/passport/google.js
@@ -1,5 +1,5 @@
 var passport = require("passport");
-var GoogleStrategy = require("passport-google-oauth").OAuthStrategy;
+const GoogleStrategy = require("passport-google-oauth").Strategy;
 
 // Use the GoogleStrategy within Passport.
 //   Strategies in passport require a `verify` function, which accept
@@ -15,6 +15,23 @@ passport.use(
     function (token, tokenSecret, profile, done) {
       console.log(profile);
       return cb(new Error("Working on it....."));
+    }
+  )
+);
+var GoogleStrategy = require("passport-google-oauth2").Strategy;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CONSUMER_KEY,
+      clientSecret: process.env.GOOGLE_CONSUMER_SECRET,
+      callbackURL: "http://yourdormain:3000/auth/google/callback",
+      passReqToCallback: true,
+    },
+    function (request, accessToken, refreshToken, profile, done) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return done(err, user);
+      });
     }
   )
 );
